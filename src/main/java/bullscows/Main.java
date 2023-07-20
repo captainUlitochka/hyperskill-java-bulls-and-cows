43717ac762aa845aa4dd25efe3c096d63c31e87c
Olga Fomenkova <olga.fomenkova@pprcard.ru>
2023-07-20 19:21:47 +0300

    Char arrays instead of int

--- src/main/java/bullscows/Main.java
+++ src/main/java/bullscows/Main.java
@@ -1,12 +1,10 @@
 package bullscows;
 
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
 
-    private static final int[] CODE = {9, 3, 0, 5}; // This value should be like this in the 2nd stage
-    private static final byte NUM_LENGTH = 4;
+    private static final char[] CODE = {'9', '3', '0', '5'}; // This value should be like this in the 2nd stage
     private static int bulls = 0;
     private static int cows = 0;
 
@@ -16,12 +14,7 @@ public class Main {
 
     private static void grade() {
         Scanner scanner = new Scanner(System.in);
-        int[] number = new int[NUM_LENGTH];
-        String[] input = scanner.nextLine().split("");
-
-        for (int i = 0; i < input.length; i++) {
-            number[i] = Integer.parseInt(input[i]);
-        }
+        char[] number = scanner.nextLine().toCharArray();
 
         // find bulls and cows
         for (int i = 0; i < CODE.length; i++) {
@@ -29,17 +22,18 @@ public class Main {
                 if (CODE[i] == number[j]) {
                     if (i == j) {
                         bulls++;
-                    } cows++;
+                    }
+                    cows++;
                 }
             }
         }
 
         if (bulls > 0) {
             cows = cows - bulls;
-            System.out.printf("Grade: %d bull(s) and %d cow(s). The secret code is %s.\n",bulls,cows, Arrays.toString(CODE).replaceAll("\\[|\\]|,|\\s", ""));
+            System.out.printf("Grade: %d bull(s) and %d cow(s). The secret code is %s.\n", bulls, cows, String.valueOf(CODE));
         } else {
-            System.out.printf("Grade: %d cow(s). The secret code is %s.\n", cows, Arrays.toString(CODE).replaceAll("\\[|\\]|,|\\s", ""));
+            System.out.printf("Grade: %d cow(s). The secret code is %s.\n", cows, String.valueOf(CODE));
         }
 
-}
+    }
 }

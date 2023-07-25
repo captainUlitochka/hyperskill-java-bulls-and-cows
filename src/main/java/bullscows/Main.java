9750291ffc84c2389afd7b61379c643aaf593e89
Olga Fomenkova <olga.fomenkova@pprcard.ru>
2023-07-25 19:23:13 +0300

    3rd stage implemented

--- src/main/java/bullscows/Main.java
+++ src/main/java/bullscows/Main.java
@@ -1,6 +1,7 @@
 package bullscows;
 
-import java.util.Scanner;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Main {
 
@@ -9,13 +10,25 @@ public class Main {
     private static int cows = 0;
 
     public static void main(String[] args) {
-        grade();
+        askForLength();
+       // print();
     }
 
-    private static void grade() {
+    private static void print() {
         Scanner scanner = new Scanner(System.in);
         char[] number = scanner.nextLine().toCharArray();
 
+        grade(number);
+
+        if (bulls > 0) {
+            cows = cows - bulls;
+            System.out.printf("Grade: %d bull(s) and %d cow(s). The secret code is %s.\n", bulls, cows, String.valueOf(CODE));
+        } else {
+            System.out.printf("Grade: %d cow(s). The secret code is %s.\n", cows, String.valueOf(CODE));
+        }
+    }
+
+    private static void grade(char[] number) {
         // find bulls and cows
         for (int i = 0; i < CODE.length; i++) {
             for (int j = 0; j < number.length; j++) {
@@ -27,13 +40,25 @@ public class Main {
                 }
             }
         }
+    }
 
-        if (bulls > 0) {
-            cows = cows - bulls;
-            System.out.printf("Grade: %d bull(s) and %d cow(s). The secret code is %s.\n", bulls, cows, String.valueOf(CODE));
+    private static void askForLength() {
+        Scanner scanner = new Scanner(System.in);
+        int length = scanner.nextInt();
+        if (length > 0 && length < 10) {
+            System.out.println("The random secret number is " + generate(length));
         } else {
-            System.out.printf("Grade: %d cow(s). The secret code is %s.\n", cows, String.valueOf(CODE));
+            System.out.println("Error: can't generate a secret number with a length of 11 because there aren't enough unique digits.");
+        }
+    }
+
+    private static String generate(int length) {
+        List<Integer> list = Arrays.asList(0, 1, 2, 3, 4, 5, 6, 7, 8, 9);
+        while (list.get(0) == 0) {
+            Collections.shuffle(list);
         }
 
+        return list.stream().limit(length).map(String::valueOf).collect(Collectors.joining(""));
     }
+
 }
